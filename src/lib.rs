7195ba110920f9ab80fa66451b5bf69ff630fe07
Quant1um <spitfirexv22@gmail.com>
2025-06-26 03:27:30 +0400

    export macros docs

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,12 @@ pub mod auv2;
 #[doc(hidden)]
 pub mod auv2 {}
 
+/// Exports an AUv2 entrypoint named `GetPluginFactoryAUV2` that wraps a global CLAP entrypoint (`clap_entry`) exported from the resulting shared library.
+/// Currently it only reexports the first plugin available through the CLAP entrypoint, but this limitation might be lifted in the future.
+///
+/// Requires `CLAP_WRAPPER_AUV2_SDK` environment variable to be set to a valid Apple AudioUnit SDK path, does nothing otherwise.
+///
+/// Failure to export a CLAP entrypoint might result in linker errors or missing symbols.
 #[macro_export]
 macro_rules! export_auv2 {
     () => {
@@ -26,6 +32,11 @@ macro_rules! export_auv2 {
     };
 }
 
+/// Exports a VST3 entrypoint named `GetPluginFactory` that wraps a global CLAP entrypoint (`clap_entry`) exported from the resulting shared library.
+///
+/// Requires `CLAP_WRAPPER_VST3_SDK` environment variable to be set to a valid VST3 SDK path (GPLv3 or Steinberg Proprietary), does nothing otherwise.
+///
+/// Failure to export a CLAP entrypoint might result in linker errors or missing symbols.
 #[macro_export]
 macro_rules! export_vst3 {
     () => {
