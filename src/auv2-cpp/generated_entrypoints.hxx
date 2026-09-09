5e7424c7586fcc4de5db373c4dbda2136c4e7d21
Quant1um <blep@amee.ee>
2026-09-09 14:24:38 +0400

    point submodules to next; update auv2 constructor, thanks @CrushedPixel

--- src/auv2-cpp/generated_entrypoints.hxx
+++ src/auv2-cpp/generated_entrypoints.hxx
@@ -1,28 +1,11 @@
 #pragma once
 #include "detail/auv2/auv2_base_classes.h"
 
-AUV2_Type typeFromAUInstance(AudioComponentInstance *ci)
-{
-    AudioComponent comp = AudioComponentInstanceGetComponent(*ci);
-    AudioComponentDescription desc;
-
-    if (comp == nullptr || AudioComponentGetDescription(comp, &desc) != 0)
-        return AUV2_Type::aumu_musicdevice;
-
-    if (desc.componentType == 'aufx')
-        return AUV2_Type::aufx_effect;
-    if (desc.componentType == 'aumi' || desc.componentType == 'aumf')
-        return AUV2_Type::aumi_noteeffect;
-
-    return AUV2_Type::aumu_musicdevice;
-}
-
 #define CLAP_AUV2_ENTRY(n)                                           \
     struct wrapAsAUV2_inst##n : free_audio::auv2_wrapper::WrapAsAUV2 \
     {                                                                \
         wrapAsAUV2_inst##n(AudioComponentInstance ci)                \
-            : free_audio::auv2_wrapper::WrapAsAUV2(                  \
-                  typeFromAUInstance(&ci), "", "", n, ci) {}         \
+            : free_audio::auv2_wrapper::WrapAsAUV2("", "", n, ci) {} \
     };                                                               \
     AUSDK_COMPONENT_ENTRY(ausdk::AUMusicDeviceFactory, wrapAsAUV2_inst##n);
 
